c82337a131a0be24608285cae76fb385cbe53059
Hassan Nazar <hassan.nazar@hreem.io>
2022-11-29 22:47:15 +0100

    fix

--- ms-testing/customer-service/src/main/java/io/hreem/casestudies/ms/testing/customer/order/model/PurchaseHistory.java
+++ ms-testing/customer-service/src/main/java/io/hreem/casestudies/ms/testing/customer/order/model/PurchaseHistory.java
@@ -1,7 +1,7 @@
 package io.hreem.casestudies.ms.testing.customer.order.model;
 
 public record PurchaseHistory(
-        int numberOfOrders,
-        int totalProductQuantity,
-        long totalAmount) {
+                int numberOfOrders,
+                int totalProductQuantity,
+                long totalAmount) {
 }
