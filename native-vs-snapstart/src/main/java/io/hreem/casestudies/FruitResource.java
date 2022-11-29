7bd8913c3781036f43bfc5830d9d49b886590e7b
Hassan Nazar <hassan.nazar@hreem.io>
2022-11-29 22:48:48 +0100

    fix

---
+++ native-vs-snapstart/src/main/java/io/hreem/casestudies/FruitResource.java
@@ -0,0 +1,31 @@
+package io.hreem.casestudies;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import java.util.List;
+
+@Path("/fruits")
+public class FruitResource {
+
+    @Inject
+    FruitSyncService service;
+
+    @GET
+    public List<Fruit> getAll() {
+        return service.findAll();
+    }
+
+    @GET
+    @Path("{name}")
+    public Fruit getSingle(String name) {
+        return service.get(name);
+    }
+
+    @POST
+    public List<Fruit> add(Fruit fruit) {
+        service.add(fruit);
+        return getAll();
+    }
+}
